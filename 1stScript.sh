4a4776d7ca71832ac57f72e870a468e4d4639e94
Polyaninartyom <polyaninartyom@mail.ru>
2016-10-06 20:00:18 +0300

    Add files via upload

--- 1stScript.sh
+++ 1stScript.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-if(($# != 4))
+if(($# != 4)) #проверка на наличие необходимых параметров
 then
    echo Необходимо 4 параметра
 else
-   directory=$1
-   ext=$2
-   di=$3
-   logfile=$4
+   directory=$1 #директория поиска
+   ext=$2       #расширенин искомых файлов
+   di=$3        #целевая аудитория
+   logfile=$4   #имя лог-файла
 
+   #записи в логе
    echo `date "+%Y.%m.%d %H:%M:%S"` "Начало работы" >> $logfile 
    echo `date "+%Y.%m.%d %H:%M:%S"` "Исходная директория: $1" >> $logfile
    echo `date "+%Y.%m.%d %H:%M:%S"` "Расширения файлов: $2" >> $logfile
    echo `date "+%Y.%m.%d %H:%M:%S"` "Целевая директория $3" >> $logfile
 
-   resultFile="$di/summar"`date "+%Y%m%d%H%M%S"`
-   touch "$resultFile"
+   resultFile="$di/summar"`date "+%Y%m%d%H%M%S"` #имя выходного файла 
+   touch "$resultFile" #создание выходного файла
 
+   #поиск файлов в директории с указанным расширений
    find $directory -name '*.'$ext | while read F
    do
-      oldhashmd5=`cat $F.md5`
-      newhashmd5=`md5sum $F | awk '{print $1}'`
+      oldhashmd5=`cat $F.md5` #считывание старого хеша из файла
+      newhashmd5=`md5sum $F | awk '{print $1}'` #получение хеша текущего файла
 
+      #проверка хешов
       if [ "$oldhashmd5" == "$newhashmd5" ]
       then
+         #построчно ситаем файл
          cat $F | while read line
          do
+            #деление строки на слова
             IFS=' ' read -r -a array <<< "$line"
-            id="${array[0]}"
-            s=0
+            id="${array[0]}" #id записи
+            s=0 #сумма чисел
 
+            #подсчет суммы числе
             for ((i=1; i<"${#array[@]}"; i++))
             do
                let s=s+${array[$i]}
             done
 
-            echo $id $s >> "$resultFile"
+            echo $id $s >> "$resultFile" #запись результата в выходной файл            
          done
+
+         #удаление файлов
+         rm -r $F.md5
+         rm -r $F
       else
          echo `date "+%Y.%m.%d %H:%M:%S"` "Для файла $F хеш не сходится" >> $logfile
       fi
--- 2ndScript.sh
+++ 2ndScript.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-if(($# == 0))
+if(($# == 0)) #проверка на наличие необходимых параметров
 then
    echo Параметр не задан
 else
-   di=$1
-   countFiles=$(expr $RANDOM % 10)
+   di=$1 #целевая аудитория
+   ext=$2 #расширение файлов
+   countFiles=$(expr $RANDOM % 10) #количество генерируемых файлов
 
    for((i=-1; i<$countFiles; i++))
    do
+      #создание файла
       suffix=$(eval date +%s)$RANDOM
-      filename="$di/file_$suffix.AAA"
+      filename="$di/file_$suffix.$ext"
       touch $filename
 
-      countLines=$(expr $RANDOM % 10)
+      countLines=$(expr $RANDOM % 10) #количество строк в файле
 
       for ((j=-1; j<$countLines; j++))
       do
-         let id=$(eval date +%s)/$(expr $RANDOM+1)
-         numbers=""
+         let id=$(eval date +%s)/$(expr $RANDOM+1) #id записи
+         numbers="" #числа
    
+         #генерация чисел
          for((k=-1; k<$(expr $RANDOM % 5); k++))
          do
             numbers="$numbers $(expr $RANDOM % 50)"
          done
 
+         #запись строки в файл
          echo $id"ID" $numbers >> "$filename"
       done
 
-      #hash-sum
+      #вычисление хеша и запись в отдельный файл
       md5sum $filename | awk '{print $1}' >> $filename.md5
    done
 fi
